4d0d4192fcae16ba972ed7e5ac6327a5e0895ae1
Samuel-Valentini <samuelvitalia@gmail.com>
2026-04-08 16:40:31 +0200

    Context name added in main

--- src/main/java/samuelvalentini/u5d2ex/U5d2exApplication.java
+++ src/main/java/samuelvalentini/u5d2ex/U5d2exApplication.java
@@ -2,12 +2,16 @@ package samuelvalentini.u5d2ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import samuelvalentini.u5d2ex.model.Table;
 
 @SpringBootApplication
 public class U5d2exApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(U5d2exApplication.class, args);
+        ConfigurableApplicationContext ctx2 = SpringApplication.run(U5d2exApplication.class, args);
+
+        System.out.println(ctx2.getBean("table3", Table.class));
 
     }
 
